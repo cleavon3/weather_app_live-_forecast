fa782233b459cfd91cc43c110d67338791c06f49
cleavon A <74119966+cleavon3@users.noreply.github.com>
2024-12-14 22:56:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import plotly.express as px
+from backend import get_data
+
+# add title, text input, slider, selection, and subheader
+st.title("Weather Forcast for the Next Days")
+place = st.text_input("Place: ")
+days = st.slider("Forcast Days", min_value=1, max_value=5, help="Select the number of forcasted days" )
+option = st.selectbox("Select data to view", ("Temperature", "Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
+
+if place:
+    # get the temperature / sky data
+    try:
+        filtered_data = get_data(place, days)
+
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperature(C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                       "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=155)
+
+    except KeyError:
+        st.write("That place does not exist.")
